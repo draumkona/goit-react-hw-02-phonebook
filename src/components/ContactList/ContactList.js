1ba8c9af3dd6379b45acfbf611e7847c1363c414
Gosia Stepaniak <g.a.stepaniak@gmail.com>
2023-06-08 12:27:27 +0200

    update

--- src/components/ContactList/ContactList.js
+++ src/components/ContactList/ContactList.js
@@ -1,7 +1,6 @@
-import React, { Component } from 'react';
+import React from 'react';
 import Contact from './Contact';
 import PropTypes from 'prop-types';
-import css from './ContactList.module.css';
 
 export const ContactList = props => {
   const { contacts, filter, deleteFunction } = props;
